4cf7d66937664e01e305e2918b1a6ecf44875493
yoshimi-takemuralab <yoshimi.kohei@lab.ime.cmc.osaka-u.ac.jp>
2020-07-24 18:03:57 +0900

    初期登録

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+//main function
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(image: AssetImage('images/alpaca_gifanime.gif')),
+        ),
+        appBar: AppBar(
+          title: Text('I am alpaca in the sky'),
+          backgroundColor: Colors.red,
+        ),
+      ),
+    );
+  }
+}
