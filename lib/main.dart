5eb0a094eb5df3997e671552b0ea52b35e6b46f0
yoshimi-takemuralab <yoshimi.kohei@lab.ime.cmc.osaka-u.ac.jp>
2020-07-24 21:19:30 +0900

    MiCard作成

--- lib/main.dart
+++ lib/main.dart
@@ -10,14 +10,75 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        body: Center(
-          child: Image(image: AssetImage('images/alpaca_gifanime.gif')),
-        ),
-        appBar: AppBar(
-          title: Text('I am alpaca in the sky'),
-          backgroundColor: Colors.red,
-        ),
+        backgroundColor: Colors.teal[900],
+        body: SafeArea(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/alpaca_gifanime.gif'),
+              ),
+              Text(
+                'Mr. Alpaca',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'takemura Labolatory',
+                style: TextStyle(
+                  fontFamily: 'Snap',
+                  fontSize: 20.0,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.0,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 300.0,
+                child: Divider(color: Colors.teal.shade100),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal[800],
+                  ),
+                  title: Text(
+                    '0120-828-828',
+                    style: TextStyle(
+                      color: Colors.teal[800],
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.mail,
+                    color: Colors.teal[800],
+                  ),
+                  title: Text(
+                    'yoshimi.kohei@flutter.ac.jp',
+                    style: TextStyle(
+                      color: Colors.teal[800],
+                    ),
+                  ),
+                ),
+              ),
+            ])),
+        //appBar: AppBar(
+        //  title: Text('I am alpaca in the sky'),
+        //  backgroundColor: Colors.teal[800],
+        //),
       ),
     );
   }
